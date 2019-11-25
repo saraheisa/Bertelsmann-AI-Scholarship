501a648b86df08a8253f5fc85332101959419836
Sara Eisa <prosarahgamal@outlook.com>
2019-11-26 01:42:02 +0200

    add inference and validation
    calculate the test loss
    calculate the train loss
    calculate the accuracy
    apply drop out
    plot the test loss and train lodd

--- fashion-mnist-classifier/model.py
+++ fashion-mnist-classifier/model.py
@@ -16,12 +16,19 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 testset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
 
+# Download and load the test data
+testset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
+
 model = nn.Sequential(nn.Linear(784, 256),
                      nn.ReLU(),
+                     nn.Dropout(p=0.5),
                      nn.Linear(256, 128),
                      nn.ReLU(),
+                     nn.Dropout(p=0.5),
                      nn.Linear(128, 64),
                      nn.ReLU(),
+                     nn.Dropout(p=0.5),
                      nn.Linear(64, 10),
                      nn.LogSoftmax(dim=1))
 
@@ -29,7 +36,11 @@ criterion = nn.NLLLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.003)
 
 epochs = 10
+train_losses, test_losses = [], []
+
 for i in range(epochs):
+    running_loss = 0
+
     for images, labels in trainloader:
         # set grade to zero
         optimizer.zero_grad()
@@ -41,6 +52,41 @@ for i in range(epochs):
         loss = criterion(out, labels)
         loss.backward()
         optimizer.step()
+
+        running_loss += loss.item()
+    else:
+        test_loss = 0
+        accuracy = 0
+        with torch.no_grad():
+            model.eval()
+            for images, labels in testloader:
+                images = images.view(images.shape[0], -1)
+                log_ps = model(images)
+                test_loss += criterion(log_ps, labels)
+                
+                ps = torch.exp(log_ps)
+                top_p, top_class = ps.topk(1, dim=1)
+                equals = top_class == labels.view(*top_class.shape)
+                accuracy += torch.mean(equals.type(torch.FloatTensor))
+        
+        model.train()
+
+        train_losses.append(running_loss/len(trainloader))
+        test_losses.append(test_loss/len(testloader))
+
+        print("Epoch: {}/{}.. ".format(i+1, epochs),
+              "Training Loss: {:.3f}.. ".format(running_loss/len(trainloader)),
+              "Test Loss: {:.3f}.. ".format(test_loss/len(testloader)),
+              "Test Accuracy: {:.3f}".format(accuracy/len(testloader)))
+
+# plot the test loss and the train loss
+
+%matplotlib inline
+%config InlineBackend.figure_format = 'retina'
+import matplotlib.pyplot as plt
+plt.plot(train_losses, label='Training loss')
+plt.plot(test_losses, label='Validation loss')
+plt.legend(frameon=False)
 
 # testing
 
