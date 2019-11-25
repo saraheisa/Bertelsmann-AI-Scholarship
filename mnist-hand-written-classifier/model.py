6c1d754ba64c9726c611c074049d57489d0e3de2
Sara Eisa <prosarahgamal@outlook.com>
2019-11-26 00:07:13 +0200

    create hand written numbers classifier using mnist

---
+++ mnist-hand-written-classifier/model.py
@@ -0,0 +1,56 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from torch import optim
+
+# Define a transform to normalize the data
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                              ])
+# Download and load the training data
+trainset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+model = nn.Sequential(nn.Linear(784, 128),
+                      nn.ReLU(),
+                      nn.Linear(128, 64),
+                      nn.ReLU(),
+                      nn.Linear(64, 10),
+                      nn.LogSoftmax(dim=1))
+
+criterion = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.003)
+
+epochs = 10
+for e in range(epochs):
+    running_loss = 0
+    for images, labels in trainloader:
+        # Flatten MNIST images into a 784 long vector
+        images = images.view(images.shape[0], -1)
+    
+        # Training pass
+        optimizer.zero_grad()
+        output = model.forward(images)
+        loss = criterion(output, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+    else:
+        print(f"Training loss: {running_loss/len(trainloader)}")
+
+# testing
+%matplotlib inline
+import helper
+
+images, labels = next(iter(trainloader))
+
+img = images[0].view(1, 784)
+# Turn off gradients to speed up this part
+with torch.no_grad():
+    logits = model.forward(img)
+
+# Output of the network are logits, need to take softmax for probabilities
+ps = F.softmax(logits, dim=1)
+helper.view_classify(img.view(1, 28, 28), ps)
