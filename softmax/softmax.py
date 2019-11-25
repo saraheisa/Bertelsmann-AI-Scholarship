db4534fbfaa038f234336753f82062253c10e05a
Sara Eisa <prosarahgamal@outlook.com>
2019-11-26 00:05:00 +0200

    softmax algorithm

---
+++ softmax/softmax.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# Write a function that takes as input a list of numbers, and returns
+# the list of values given by the softmax function.
+def softmax(L):
+    expL = np.exp(L)
+    expSum = expL.sum()
+    return np.divide (expL, expSum)
